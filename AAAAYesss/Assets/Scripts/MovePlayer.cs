4c0e6a3b4a70ef33cd572b990190878491d5ef29
5FeetUnder <petschbenjamin@gmail.com>
2016-01-30 00:35:58 +0100

    Basic Movement added

---
+++ AAAAYesss/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+    public int playerNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (playerNumber){
+            case 1:
+                transform.position += new Vector3(Input.GetAxisRaw("player1Horizontal"), Input.GetAxisRaw("player1Vertical"));
+                break;
+            case 2:
+                transform.position += new Vector3(Input.GetAxisRaw("player2Horizontal"), Input.GetAxisRaw("player2Vertical"));
+                break;
+            case 3:
+                transform.position += new Vector3(Input.GetAxisRaw("player3Horizontal"), Input.GetAxisRaw("player3Vertical"));
+                break;
+            case 4:
+                transform.position += new Vector3(Input.GetAxisRaw("player4Horizontal"), Input.GetAxisRaw("player4Vertical"));
+                break;
+        }
+    }
+}
