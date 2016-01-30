a9391cffc7e26ca3275dcd2948c761a801f0c436
Sergio R. Lumley <lumley256@gmail.com>
2016-01-30 13:18:09 +0100

    Created main scene with spawning points

---
+++ AAAAYesss/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    private const int MAX_PLAYER_COUNT = 4; 
+
+	void Awake(){
+        var gameManager = GameManager.Instance;
+        
+        var playerCount = gameManager != null ? gameManager.PlayerCount : 2;
+        
+        for (int i=0; i< 4; ++i){
+             string spawningPointTag = "spawnPoint" + (i + 1);
+             var spawningPoint = GameObject.FindGameObjectWithTag(spawningPointTag);
+             spawningPoint.SetActive(i < playerCount);
+        }
+        
+    }
+}
---
+++ AAAAYesss/Assets/Scripts/SpawningPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpawningPoint : MonoBehaviour {
+
+	public GameObject gameObjectToSpawn;
+    
+    void Start(){
+        
+        if(gameObjectToSpawn != null){
+            Instantiate(gameObjectToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}
