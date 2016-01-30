91df9a094dbfedaf14b929b0110e576de361f63f
5FeetUnder <petschbenjamin@gmail.com>
2016-01-30 22:25:51 +0100

    updated forcepush

--- AAAAYesss/Assets/Scripts/ForcePush.cs
+++ AAAAYesss/Assets/Scripts/ForcePush.cs
@@ -5,48 +5,67 @@ using System.Collections;
 public class ForcePush : MonoBehaviour {
     public float range;
     public float strength;
+    public int cooldown; //Cooldown in frames
+    public int drainSpeed; //Drain speed per frame
+
+    private bool isForceEnabled;
     private int playerNumber;
+    private int cooldownCounter;
+    private int playerCount;
 
-    private Rigidbody2D playerRigidBody;
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         playerNumber = GetComponent<Player>().playerId;
-        playerRigidBody = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+        isForceEnabled = false;
+        cooldownCounter = cooldown;
+        var gameManager = GameManager.Instance;
+        playerCount = gameManager != null ? gameManager.PlayerCount : 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         string playerIdentifier = "player" + playerNumber;
         //Debug.Log(Input.GetButton(playerIdentifier + "Submit"));
-        if (Input.GetButton(playerIdentifier + "Submit"))
+        isForceEnabled = Input.GetButton(playerIdentifier + "Submit");
+        if (Input.GetButton(playerIdentifier + "Submit") && cooldownCounter >= 0)
         {
-            for(int i = 1; i <= 4; i++)
+            if (cooldownCounter == cooldown)
             {
-                if (playerNumber != i)
-                    forceField(i);
-            }
-        }
-	}
-
-    void forceField (int player) {
-        string targetName = "player" + player;
-        var target = GameObject.FindGameObjectWithTag(targetName);
-        if (target != null)
-        {
-            Vector2 direction = target.transform.position - transform.position;
-            direction.Normalize();
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, range, player + 7);
-            if (hit.collider != null)
-            {
-                Rigidbody2D targetRigidBody = target.GetComponent<Rigidbody2D>();
-                //Rigidbody2D targetRigidBody = hit.rigidbody;
-                if (targetRigidBody != null)
+                Debug.Log("playercount " + playerCount);
+                for (int i = 1; i <= playerCount; i++)
                 {
-                    Vector3 direction3d = new Vector3(direction.x, direction.y);
-                    targetRigidBody.MovePosition((target.transform.position + direction3d) * strength);
+                    if (!(i == playerNumber))
+                        forceField(i);
                 }
             }
+            if (cooldownCounter >= drainSpeed)
+                cooldownCounter -= drainSpeed;
+            else
+                cooldownCounter = 0;
+        }
+        else if (cooldownCounter < cooldown)
+        {
+            cooldownCounter++;
+        }
+    }
+
+    void forceField (int player) {
+        //volatile GameObject target;
+        var target = GameObject.FindGameObjectsWithTag("player" + player);
+        if (target[0] != null)
+        {   
+            Rigidbody2D targetBody = target[0].GetComponent<Rigidbody2D>();
+            Vector2 direction = target[0].transform.position - this.transform.position;
+            Vector3 direction3d = new Vector3(direction.x, direction.y);
+            float magnitude = direction.magnitude;
+            if (magnitude <= range && targetBody != null)
+            {
+                Debug.Log("Ranger " + playerNumber + player);
+                direction.Normalize();
+                targetBody.MovePosition((target[0].transform.position + direction3d) * strength);
+            }
+            
         }
     }
 }
