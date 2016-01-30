4258834c5fa1e1b9d57e4ffea009090c3cd092b9
Sergio R. Lumley <lumley256@gmail.com>
2016-01-30 23:26:38 +0100

    Players will now die in enemy bases

--- AAAAYesss/Assets/Scripts/KillingRegion.cs
+++ AAAAYesss/Assets/Scripts/KillingRegion.cs
@@ -2,56 +2,58 @@
 using System.Collections.Generic;
 using System;
 
-public class KillingRegion : MonoBehaviour {
+public class KillingRegion : MonoBehaviour
+{
 
     private static string COLLIDING_TAG = "player";
-	public float secondsToKill = 3.0f;
-    
+    public float secondsToKill = 3.0f;
+
     public int sparePlayerWithId = 0;
-    
-    private Dictionary<string, float> collidingPlayers = new Dictionary<string, float>();
-    
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.tag.StartsWith(COLLIDING_TAG)){
+
+    private float[] collidingPlayerValueArray = new float[4];
+    private bool[] isPlayerCollidingArray = new bool[4];
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.StartsWith(COLLIDING_TAG))
+        {
             var playerScript = other.GetComponent<Player>();
-            if(playerScript.playerId != sparePlayerWithId && !collidingPlayers.ContainsKey(other.tag)){
-                collidingPlayers.Add(other.tag, 0.0f);
-            }
-        }
-    }
-    
-    void FixedUpdate(){
-        List<string> playersKilled = null;
-        foreach(var key in collidingPlayers.Keys){
-            if(ShouldKillPlayer(key, Time.fixedDeltaTime)){
-                if(playersKilled == null){
-                    playersKilled = new List<string>();
-                }
-                
-                playersKilled.Add(key);
-            }        
-        }
-        
-        if(playersKilled != null){
-            foreach(var key in playersKilled){
-                var otherPlayer = GameObject.FindGameObjectWithTag(key);
-                if(otherPlayer != null){
-                    otherPlayer.GetComponent<Player>().KillPlayer();
-                }
-                collidingPlayers.Remove(key);
+            if (playerScript.playerId != sparePlayerWithId && !isPlayerCollidingArray[playerScript.playerId - 1])
+            {
+                isPlayerCollidingArray[playerScript.playerId - 1] = true;
             }
         }
     }
 
-    private bool ShouldKillPlayer(string tag, float deltaTime)
-    {        
-        collidingPlayers[tag] += deltaTime;
-        return collidingPlayers[tag] > secondsToKill;
+    void FixedUpdate()
+    {
+        float deltaTime = Time.fixedDeltaTime;
+        for (int i = 0; i < isPlayerCollidingArray.Length; ++i)
+        {
+            if (isPlayerCollidingArray[i])
+            {
+                collidingPlayerValueArray[i] += deltaTime;
+                if(collidingPlayerValueArray[i] > secondsToKill){
+                    isPlayerCollidingArray[i] = false;
+                    collidingPlayerValueArray[i] = 0;
+                    
+                    var dyingTarget = GameObject.FindGameObjectWithTag(COLLIDING_TAG + (i + 1));
+                    if(dyingTarget != null){
+                        var playerScript = dyingTarget.GetComponent<Player>();
+                        playerScript.KillPlayer();
+                    }
+                }
+            }
+        }
     }
 
-    void OnTriggerExit2D(Collider2D other){
-        if(other.tag.StartsWith(COLLIDING_TAG) && collidingPlayers.ContainsKey(other.tag)){
-            collidingPlayers.Remove(tag);
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.StartsWith(COLLIDING_TAG))
+        {
+            var playerScript = other.GetComponent<Player>();
+            isPlayerCollidingArray[playerScript.playerId - 1] = false;
+            collidingPlayerValueArray[playerScript.playerId - 1] = 0;
         }
     }
 }
