048c12774775e6d936ecd0936380b879ab1a6c04
5FeetUnder <petschbenjamin@gmail.com>
2016-01-30 11:52:34 +0100

    made movement senseful, modified player colliders, made animation work exact, remove mousbuttons from input

--- AAAAYesss/Assets/Scripts/MovePlayer.cs
+++ AAAAYesss/Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class MovePlayer : MonoBehaviour {
 
     public int playerNumber;
-    public int velocityBorder; //determines teh maximum value of velocity for the player to be considered standing/idling
     public float speed = 5.0f;
     
     private Rigidbody2D playerRigidbody;
 
+    private Animator playerAnimator;
+
 	// Use this for initialization
 	void Start () {
-	   playerRigidbody = GetComponent<Rigidbody2D>();
-	}
+	    playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = this.gameObject.GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,33 +22,31 @@ public class MovePlayer : MonoBehaviour {
         var movement = new Vector3(Input.GetAxisRaw(playerIdentifier + "Horizontal"), Input.GetAxisRaw(playerIdentifier + "Vertical"));
         
         movement = movement.normalized * speed * Time.deltaTime;
-        
-        playerRigidbody.MovePosition (transform.position + movement);
 
-        setAnimation(playerIdentifier);
-        Debug.Log(Input.GetAxisRaw(playerIdentifier + "Horizontal"));
+        playerRigidbody.MovePosition (transform.position + movement);
+        //playerRigidbody.AddForce(movement);
+
+        setAnimation(movement);
     }
 
-    void setAnimation(string playerIdentifier) {
-        //Vertical
-        if(Input.GetAxisRaw(playerIdentifier + "Vertical") > 0)
-            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 4);
-        if(Input.GetAxisRaw(playerIdentifier + "Vertical") < 0)
-            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 3);
-        //Horizontal
-        if (Input.GetAxisRaw(playerIdentifier + "Horizontal") > 0)
-            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 1);
-        if(Input.GetAxisRaw(playerIdentifier + "Horizontal") < 0)
-            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 2);
-        //when player moves diagonally, only the horizontal part of it is animated (modifiable by order of //Vertical and //Horizontal
-        float velocityX = playerRigidbody.velocity.x;
-        float velocityY = playerRigidbody.velocity.y;
-        float realVelocity = Mathf.Sqrt((velocityX * velocityX) + (velocityY * velocityY)); //Pythagoras
-        if(
-           realVelocity < velocityBorder ||
-           (Input.GetAxisRaw(playerIdentifier + "Horizontal") == 0 &&
-           Input.GetAxisRaw(playerIdentifier + "Vertical") == 0)
-          )
-            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 0);
+    void setAnimation(Vector3 movement) {
+        
+        if (movement.x == 0 && movement.y == 0)
+            playerAnimator.SetInteger("Direction", 0);
+        else if (Mathf.Abs(movement.x) >= Mathf.Abs(movement.y))
+        {
+            if (movement.x > 0)
+                playerAnimator.SetInteger("Direction", 1);
+            else if(movement.x < 0)
+                playerAnimator.SetInteger("Direction", 2);
+        }
+        else
+        {
+            if (movement.y > 0)
+                playerAnimator.SetInteger("Direction", 4);
+            else if(movement.y < 0)
+                playerAnimator.SetInteger("Direction", 3);
+        }
+        
     }
 }
