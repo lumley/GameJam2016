d497ea817a7d795779ed02456c630e125602b0e6
Sergio R. Lumley <lumley256@gmail.com>
2016-01-30 15:58:09 +0100

    Added lava, detecting collision with it

---
+++ AAAAYesss/Assets/Scripts/PlayerKiller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKiller : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+        if(other.tag.StartsWith("player")){
+            // Kill player
+            
+            
+            
+        }
+    }
+}
