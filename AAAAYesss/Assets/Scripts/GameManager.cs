1b88488fdcedc81a69e0ced59fac25889b61037c
Sergio R. Lumley <lumley256@gmail.com>
2016-01-30 12:35:25 +0100

    Creating main screen and game scene

---
+++ AAAAYesss/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+    
+    public int PlayerCount { get; set; }
+
+	void Awake  () {
+        
+        if(Instance == null){
+            Instance = this;
+        }else if(Instance != this){
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(this);
+	}
+    
+    public void StartGameWithTwoPlayers(){
+        InitializeGameWithPlayers(2);
+    }
+    
+    public void StartGameWithThreePlayers(){
+        InitializeGameWithPlayers(3);
+    }
+    
+    public void StartGameWithFourPlayers(){
+        InitializeGameWithPlayers(4);
+    }
+    
+    private void InitializeGameWithPlayers(int playerCount){
+        PlayerCount = playerCount;
+        SceneManager.LoadScene("GameScene");
+    }
+}
