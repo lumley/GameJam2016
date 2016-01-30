aca721df2d763b0532f1b675cd5c57cda37bb8c0
5FeetUnder <petschbenjamin@gmail.com>
2016-01-30 04:37:16 +0100

    MovePlayer.cs now communicates with Animator
    THEY ARE ALIVE!!!

--- AAAAYesss/Assets/Scripts/MovePlayer.cs
+++ AAAAYesss/Assets/Scripts/MovePlayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MovePlayer : MonoBehaviour {
 
     public int playerNumber;
+    public int velocityBorder; //determines teh maximum value of velocity for the player to be considered standing/idling
     public float speed = 5.0f;
     
     private Rigidbody2D playerRigidbody;
@@ -21,5 +22,31 @@ public class MovePlayer : MonoBehaviour {
         movement = movement.normalized * speed * Time.deltaTime;
         
         playerRigidbody.MovePosition (transform.position + movement);
+
+        setAnimation(playerIdentifier);
+        Debug.Log(Input.GetAxisRaw(playerIdentifier + "Horizontal"));
+    }
+
+    void setAnimation(string playerIdentifier) {
+        //Vertical
+        if(Input.GetAxisRaw(playerIdentifier + "Vertical") > 0)
+            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 4);
+        if(Input.GetAxisRaw(playerIdentifier + "Vertical") < 0)
+            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 3);
+        //Horizontal
+        if (Input.GetAxisRaw(playerIdentifier + "Horizontal") > 0)
+            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 1);
+        if(Input.GetAxisRaw(playerIdentifier + "Horizontal") < 0)
+            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 2);
+        //when player moves diagonally, only the horizontal part of it is animated (modifiable by order of //Vertical and //Horizontal
+        float velocityX = playerRigidbody.velocity.x;
+        float velocityY = playerRigidbody.velocity.y;
+        float realVelocity = Mathf.Sqrt((velocityX * velocityX) + (velocityY * velocityY)); //Pythagoras
+        if(
+           realVelocity < velocityBorder ||
+           (Input.GetAxisRaw(playerIdentifier + "Horizontal") == 0 &&
+           Input.GetAxisRaw(playerIdentifier + "Vertical") == 0)
+          )
+            this.gameObject.GetComponent<Animator>().SetInteger("Direction", 0);
     }
 }
