41210d2723abf1fb916377fa6b37f306a12131f7
5FeetUnder <petschbenjamin@gmail.com>
2016-01-31 14:43:00 +0100

    Made begin scene

---
+++ AAAAYesss/Assets/Scripts/EventSystemManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EventSystemManager : MonoBehaviour {
+
+    public int playerCount;
+
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("AD");
+        startGame();
+    }
+
+    private void startGame()
+    {
+        switch (playerCount)
+        {
+            case 2:
+                GameManager.Instance.StartGameWithTwoPlayers();
+                break;
+            case 3:
+                GameManager.Instance.StartGameWithThreePlayers();
+                break;
+            case 4:
+                GameManager.Instance.StartGameWithFourPlayers();
+                break;
+        }
+    }
+}
