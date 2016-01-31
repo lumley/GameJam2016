bf259df4a7db845ffc036456dec0c9d44aec9370
5FeetUnder <petschbenjamin@gmail.com>
2016-01-31 01:00:40 +0100

    added some assets, made forcepush work, fixed Input

--- AAAAYesss/Assets/Scripts/ForcePush.cs
+++ AAAAYesss/Assets/Scripts/ForcePush.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 [RequireComponent(typeof(Player))]
 public class ForcePush : MonoBehaviour {
@@ -8,62 +9,91 @@ public class ForcePush : MonoBehaviour {
     public int cooldown; //Cooldown in frames
     public int drainSpeed; //Drain speed per frame
 
-    private bool isForceEnabled;
     private int playerNumber;
     private int cooldownCounter;
     private int playerCount;
+    private bool isRecharging;
 
     // Use this for initialization
     void Start () {
         playerNumber = GetComponent<Player>().playerId;
-        isForceEnabled = false;
+        isRecharging = false;
         cooldownCounter = cooldown;
         var gameManager = GameManager.Instance;
         playerCount = gameManager != null ? gameManager.PlayerCount : 2;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         string playerIdentifier = "player" + playerNumber;
         //Debug.Log(Input.GetButton(playerIdentifier + "Submit"));
-        isForceEnabled = Input.GetButton(playerIdentifier + "Submit");
-        if (Input.GetButton(playerIdentifier + "Submit") && cooldownCounter >= 0)
+        if (cooldownCounter == cooldown)
+            isRecharging = false;
+        if (Input.GetButton(playerIdentifier + "Submit"))
         {
-            if (cooldownCounter == cooldown)
-            {
-                Debug.Log("playercount " + playerCount);
-                for (int i = 1; i <= playerCount; i++)
-                {
-                    if (!(i == playerNumber))
-                        forceField(i);
-                }
-            }
-            if (cooldownCounter >= drainSpeed)
-                cooldownCounter -= drainSpeed;
-            else
-                cooldownCounter = 0;
+            if(!isRecharging)
+                pushForce();
+            else 
+                playTiltAnimation();
         }
-        else if (cooldownCounter < cooldown)
+        else
+        {
+            if (cooldownCounter < cooldown)
+                isRecharging = true;
+        }
+
+        if (cooldownCounter < cooldown)
         {
             cooldownCounter++;
         }
     }
 
+    private void playTiltAnimation()
+    {
+        //TODO: Implement it
+    }
+
+    private void pushForce()
+    {
+        if (cooldownCounter > 0)
+        {
+            Debug.Log("playercount " + playerCount);
+            for (int i = 1; i <= playerCount; i++)
+            {
+                if (!(i == playerNumber))
+                    forceField(i);
+            }
+            if (cooldownCounter >= drainSpeed)
+            {
+                cooldownCounter -= drainSpeed;
+            }
+            else
+            {
+                cooldownCounter = 0;
+                isRecharging = true;
+
+            }
+        }
+        
+    }
+
     void forceField (int player) {
-        //volatile GameObject target;
-        var target = GameObject.FindGameObjectsWithTag("player" + player);
-        if (target[0] != null)
+        var target = GameObject.FindGameObjectWithTag("player" + player);
+        if (target != null)
         {   
-            Rigidbody2D targetBody = target[0].GetComponent<Rigidbody2D>();
-            Vector2 direction = target[0].transform.position - this.transform.position;
+            Rigidbody2D targetBody = target.GetComponent<Rigidbody2D>();
+            Vector2 direction = target.transform.position - this.transform.position;
             Vector3 direction3d = new Vector3(direction.x, direction.y);
             float magnitude = direction.magnitude;
             if (magnitude <= range && targetBody != null)
             {
                 Debug.Log("Ranger " + playerNumber + player);
-                direction.Normalize();
-                targetBody.MovePosition((target[0].transform.position + direction3d) * strength);
+                direction3d.Normalize();
+                Vector3 test = target.transform.position + (direction3d * strength);
+                targetBody.MovePosition(test);
+                if (target.transform.position == test)
+                    Debug.Log("Succesful pushed");
             }
             
         }
