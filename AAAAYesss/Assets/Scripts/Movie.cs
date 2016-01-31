5134200e00454b311ed9d405e99cd12d86404fe8
PrincessEleni <eleni_ver@hotmail.com>
2016-01-31 15:27:46 +0200

    Final image

--- AAAAYesss/Assets/Scripts/Movie.cs
+++ AAAAYesss/Assets/Scripts/Movie.cs
@@ -13,6 +13,7 @@ public class Movie : MonoBehaviour {
 	public Sprite image5; //Drag your first sprite here in inspector.
 	public Sprite image6; //Drag your first sprite here in inspector.
 	public Sprite image7; //Drag your first sprite here in inspector.
+	public Sprite image8; //Drag your first sprite here in inspector.
 
 	private int countPage;
 
@@ -54,6 +55,8 @@ public class Movie : MonoBehaviour {
 		} else if(countPage==6){
 			myImageComponent.sprite = image7;
 		} else if(countPage==7){
+			myImageComponent.sprite = image8;
+		} else if(countPage==8){
 			//GAME STARTS
 			SceneManager.LoadScene("MainScene");
 		}
