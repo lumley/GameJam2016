bb3a3eb53f7f885f1a78d5418b38bd79888ba0f3
Sergio R. Lumley <lumley256@gmail.com>
2016-01-31 06:27:11 +0100

    Created UI for in game items

--- AAAAYesss/Assets/Scripts/SpawningPoint.cs
+++ AAAAYesss/Assets/Scripts/SpawningPoint.cs
@@ -15,6 +15,7 @@ public class SpawningPoint : MonoBehaviour {
 	public GameObject gameObjectToSpawn;
     private string[] objectsToCollect = new string[0];
     private bool[] isItemCollectedArray = new bool[0];
+    private GameObject[] itemsToCollectUi = new GameObject[0];
     
     private Collider2D colliderReference;
     
@@ -29,10 +30,13 @@ public class SpawningPoint : MonoBehaviour {
             
             objectsToCollect = new string[objectsToCollectCount];
             isItemCollectedArray = new bool[objectsToCollectCount];
+            itemsToCollectUi = new GameObject[objectsToCollectCount];
             
             for(int i=0; i< objectsToCollectCount; ++i){
                 int index = UnityEngine.Random.Range(0, gameItems.Count);
-                objectsToCollect[i] = gameItems[index].name;
+                var item = gameItems[index];
+                objectsToCollect[i] = item.name;
+                itemsToCollectUi[i] = CreateUiElement(item, i);
                 gameItems.RemoveAt(index);
             }
         }
@@ -42,13 +46,25 @@ public class SpawningPoint : MonoBehaviour {
         }
     }
 
-	public void DroppedItem(GameObject item) 
+    private GameObject CreateUiElement(GameObject item, int i)
+    {
+        var newPosition = transform.position + new Vector3(-1.5f + 1.5f * (i % 3), 1.5f * ((i / 3) + 1), Player.PLAYER_SPAWNING_DEPTH);
+        GameObject newItem = (GameObject) Instantiate(item, newPosition, Quaternion.identity);
+        newItem.GetComponent<Collider2D>().enabled = false;
+        newItem.GetComponent<SpriteRenderer>().color = Color.red;
+        newItem.transform.parent = transform;
+        return newItem;
+    }
+
+    public void DroppedItem(GameObject item) 
 	{
 		if (item.CompareTag (ITEM_TAG) && IsWithinBounds(item))
 		{
 			int index = FindIndexOf(objectsToCollect, item.name);
             if(index >= 0){
                 isItemCollectedArray[index] = true;
+                var spriteRenderer = itemsToCollectUi[index].GetComponent<SpriteRenderer>();
+                spriteRenderer.enabled = false;
                 CheckIfCanSummonDemon();
             }
 		}
@@ -60,6 +76,8 @@ public class SpawningPoint : MonoBehaviour {
             int index = FindIndexOf(objectsToCollect, item.name);
             if(index >= 0){
                 isItemCollectedArray[index] = false;
+                var spriteRenderer = itemsToCollectUi[index].GetComponent<SpriteRenderer>();
+                spriteRenderer.enabled = true;
             }
 		}
     }
